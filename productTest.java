8761c2c3ac07b42b995c304213b1d46962d0033e
alexjimv <113520850+alexjimv@users.noreply.github.com>
2022-09-14 16:40:58 +0300

    Add files via upload
    Εξεταστική Σεπτεμβρίου 2022

---
+++ productTest.java
@@ -0,0 +1,22 @@
+@Before 
+public class ProductTester {
+	@Before
+    public void setUp() {
+        String product = new product();
+    }
+
+@Test
+public void product() {
+ List <array> mockDb = new List<array> 
+ List<String> actual = Arrays.asList("GR", "12AEW", "433A");
+ List<String> expected = Arrays.asList("GR", "12AE2", "433A");
+ assertThat(actual, is(expected));
+ return true;
+ }
+
+@After
+        public void tearDown(){
+   product = null;
+   List <array> = null;
+   }
+}
